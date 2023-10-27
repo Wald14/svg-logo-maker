a6931f5707b2ad4975f57209a6236c16841aadbd
Wald14 <lukewald@hotmail.com>
2023-10-26 23:08:37 -0500

    played around with parent and sub classes for the shape. Currently it is far from optamized but functional. Wrote code to write the SVG file based off user input. Currently has an error where it is coming back with an undefined color for the shape.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Instructions
   1. git init
-  2. npm install
+  2. npm install inquirer
   3. npm add inquirer-max-length-input-prompt
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {generateSVG} = require('./lib/shapes.js')
 
 // Add package to allow a limit to the string length that inquirer accepts
 //  https://www.npmjs.com/package/inquirer-maxlength-input-prompt
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
-// const generateSVG = require('./utils/generateSVG.js')
 
 // Questions for user
 const questions = [
@@ -16,13 +16,53 @@ const questions = [
     message: 'Enter up to 3 characters',
     name: 'chars',
     maxLength: 3
+  },
+  {
+    type: 'input',
+    message: 'What color do you want the characters?',
+    name: 'charsColor'
+  },
+  {
+    type: 'list',
+    message: 'Choose a shape:',
+    name: 'shape',
+    choices: [
+      {
+        name: 'Square',
+        value: 'square'
+      },
+      {
+        name: 'Circle',
+        value: 'circle'
+      },
+      {
+        name: 'Triangle',
+        value: 'triangle'
+      }
+    ]
+  },
+  {
+    type: 'input',
+    message: 'What color do you want the shape to be?',
+    name: 'shapeColor' 
   }
 ]
 
+
+// Function that writes to SVG file
+function writeToSVG(filename, svgCode){
+  fs.writeFile(filename, svgCode, (err) => {
+    if (err) console.log("Error with writing SVG file")
+  })
+}
+
+
+//  Function that initializes the app
 function init() {
   inquirer.prompt(questions)
   .then(answers => {
-    console.log(answers)
+    let svgCode = generateSVG(answers);
+    writeToSVG('./new_SVG_file/logo.svg', svgCode)
   })
 }
 
